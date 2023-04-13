631aafd9728c062df301fa9fb355ee2e9a090b4c
Vinayak Kapoor <krrishkapoor97@gmail.com>
2023-04-13 01:57:29 -0700

    scroll behaviour updated

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import Home from "./pages/Home";
 import Pricing from "./pages/Pricing";
 import Services from "./pages/Services";
 import Gallery from "./pages/Gallery";
+import ScrollToTop from "./components/scroll-behaviour/ScrollToTop";
 function App() {
     return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/gallery" element={<Gallery />} />
-            <Route path="/pricing" element={<Pricing />} />
-            <Route path="/services" element={<Services />} />
-        </Routes>
+        <>
+            <ScrollToTop />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/gallery" element={<Gallery />} />
+                <Route path="/pricing" element={<Pricing />} />
+                <Route path="/services" element={<Services />} />
+            </Routes>
+        </>
     );
 }
 
